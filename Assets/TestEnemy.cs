808d9709ba3238e550c63abf854a495f1c2d4333
Crazykrai <ayushverma8@gmail.com>
2024-02-02 13:53:16 -0500

    Added player input

---
+++ Assets/TestEnemy.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class TestEnemy : MonoBehaviour
+{
+
+    private Rigidbody2D rb;
+    private Vector2 movementVector;
+    private float movementX;
+    private float movementY;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void OnMove(InputValue movementValue)
+    {
+        movementVector = movementValue.Get<Vector2>();
+
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movementVector * speed * Time.fixedDeltaTime);
+    }
+}
