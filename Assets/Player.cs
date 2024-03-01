55da4280f47862f10c1a0a2d1c3513960afeb888
13rody <60441650+13rody@users.noreply.github.com>
2024-03-01 15:53:37 -0500

    Brody commit
    updated movement, tile collision and started implementing health

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -11,16 +11,31 @@ public class Player : MonoBehaviour
     private Rigidbody2D rb;
     public Animator animator;
     private Vector3 mousePosition;
-
+    private Vector3 inputVector;
+    public SpriteRenderer sr;
+    private HealthBarController hbc;
     private void Start()
     {
+       sr = GetComponent<SpriteRenderer>();
        rb = GetComponent<Rigidbody2D>();
+       hbc = GetComponent<HealthBarController>();
     }
     void FixedUpdate()
     {
         animator.SetFloat("speed", (Mathf.Abs(moveInput.y)+Mathf.Abs(moveInput.x)/2));
         rb.MovePosition(rb.position + moveInput * moveSpeed * Time.fixedDeltaTime);
-        mousePosition = Input.mousePosition;
+        mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        inputVector = mousePosition - rb.transform.position;
+        inputVector = inputVector.normalized;
+        if (mousePosition.x > rb.position.x)
+        {
+            sr.flipX = false;
+        }
+        else
+        {
+            sr.flipX = true;
+        }
+
     }
     void OnMove(InputValue value)
     {
@@ -29,7 +44,7 @@ public class Player : MonoBehaviour
 
     void OnFire()
     {
-        print("click!");
-            animator.SetTrigger("click");
+        animator.SetTrigger("click");
+            
     }
 }
